bfee91afde85819c5804cf9d77f0f449c033b19a
HtetOoNaing <htetoonaing.ycc@gmail.com>
2020-11-06 11:43:50 +0630

    connect to local db

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const app = express();
 const tourRouter = require('./routes/tourRoutes');
 // middleware
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 //custom middleware
-app.use((req, res, next) => {
-    console.log('Hello from the middleware!');
-    next();
-});
+// app.use((req, res, next) => {
+//     console.log('Hello from the middleware!');
+//     next();
+// });
 
 // routes
 app.use('/api/v1/tours', tourRouter);
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const app = require('./app');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
-const port = 8000;
+dotenv.config({path: './config.env'});
+
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => console.log('DB connection successful!'));
+
+const port = process.env.port || 8000;
 app.listen(port,() => {
     console.log(`App running on port ${port} ...`);
 })

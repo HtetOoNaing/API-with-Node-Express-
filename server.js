5c73783937eeffae1c6ae0bab0c7193b13e9a8ac
HtetOoNaing <htetoonaing.ycc@gmail.com>
2020-11-07 21:29:25 +0630

    connect to db and create tour schema

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
-dotenv.config({path: './config.env'});
+dotenv.config({ path: './config.env' });
 
 mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
@@ -10,7 +10,25 @@ mongoose.connect(process.env.DATABASE, {
     useFindAndModify: false
 }).then(() => console.log('DB connection successful!'));
 
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price']
+    }
+});
+
+const Tour = mongoose.model('Tour', tourSchema)
+
 const port = process.env.port || 8000;
-app.listen(port,() => {
+app.listen(port, () => {
     console.log(`App running on port ${port} ...`);
 })

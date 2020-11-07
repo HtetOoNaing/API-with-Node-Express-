ed42a05c25ddcde5e2750d68720c527a00a63aa5
HtetOoNaing <htetoonaing.ycc@gmail.com>
2020-11-08 00:00:55 +0630

    update and delete document form db

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -49,13 +49,33 @@ exports.createTour = async (req,res) => {
     }
     
 }
-exports.updateTour = (req,res) => {
-    const id = req.params.id * 1;
-    // update tour here
-    res.status(200).json({ status: 'success', data: {  }});
+exports.updateTour = async (req,res) => {
+    try {
+        const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+        res.status(200).json({ 
+            status: 'success', 
+            data: { tour }
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
+    
 }
-exports.deleteTour = (req,res) => {
-    const id = req.params.id * 1;
-    // delete tour here
-    res.status(200).json({ status: 'success', data: {  }});
+exports.deleteTour = async (req,res) => {
+    try {
+        await Tour.findByIdAndDelete(req.params.id);
+        res.status(204).json({ status: 'success', data: null });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
+    
 }
